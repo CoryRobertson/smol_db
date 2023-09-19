6bb5f1f0151137aacdfd002839e3e64bd4a12446
coryrobertson <cory.robertson17@gmail.com>
2023-09-19 10:32:19 -0700

    cargo clippy

--- smol_db_common/src/encryption/mod.rs
+++ smol_db_common/src/encryption/mod.rs
@@ -4,7 +4,7 @@ use rsa::rand_core::OsRng;
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 
 /// The length of bits an rsa key will be
-pub(self) const BIT_LENGTH: usize = 2048;
+const BIT_LENGTH: usize = 2048;
 pub mod client_encrypt;
 pub mod encrypted_data;
 pub mod server_encrypt;
@@ -17,11 +17,11 @@ pub enum EncryptionError {
 }
 
 /// Encrypt a piece of data using a public key
-pub(self) fn encrypt(key: &RsaPublicKey, mut rng: &mut OsRng, msg: &[u8]) -> rsa::Result<Vec<u8>> {
+fn encrypt(key: &RsaPublicKey, mut rng: &mut OsRng, msg: &[u8]) -> rsa::Result<Vec<u8>> {
     key.encrypt(&mut rng, Pkcs1v15Encrypt, msg)
 }
 
 /// Decrypt a piece of data using a private key
-pub(self) fn decrypt(pri_key: &RsaPrivateKey, enc_data: &[u8]) -> rsa::Result<Vec<u8>> {
+fn decrypt(pri_key: &RsaPrivateKey, enc_data: &[u8]) -> rsa::Result<Vec<u8>> {
     pri_key.decrypt(Pkcs1v15Encrypt, enc_data)
 }
